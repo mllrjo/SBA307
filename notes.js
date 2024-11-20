edb17c9b080971fe897d2b0237fbc02b515cc743
mllrjo <159646777+mllrjo@users.noreply.github.com>
2024-11-20 14:08:54 -0500

    initial commit

---
+++ notes.js
@@ -0,0 +1,21 @@
+// Kw: Functional Purposeful
+
+// Aid: Spacing Sizing/ TOur Guide Experience
+
+// Must: Navigation Menu /Accessibilty
+
+// Ow: Simple Elegant/ Attention Based Design[Component Based -sideNav]
+
+// Theme Based
+
+// Sw: Auto Adjusting/ Snap into Place 
+// -> Dont wanna lose features for device size
+
+// Nai: Compact Side Menu.. Passive Experience vs Active Experience -> Industry Standard
+
+// xX: Color Sync - dotdash.io => Engagement of site
+// Reactive 
+
+// Rz: Disappearing Divs - COmpartmentalized -Creating the experience w visual options -fontAdjustment
+
+// Ath: Customer Introspection.Appeealing to Customer based on Customers Wants - Accessibility -EAsy to find
